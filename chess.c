a58360c60945c308a1e5478bd3994dc5f2488068
aurelialacroixip <albinawolf0@gmail.com>
2018-02-28 19:13:54 +0700

    Fixing chess board

--- chess.c
+++ chess.c
@@ -1,52 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main ()
-{
-printf(" —-------------------------------—-- ");
-printf("\n");
-printf("| 1 | R | H | E | Q | K | E | H | R |");
-printf("\n");
-printf(" —-------------------------------—-- ");
-printf("\n");
-printf("| 2 | P | P | P | P | P | P | P | P |");
-printf("\n");
-printf(" —-------------------------------—-- ");
-printf("\n");
-
-printf("| 3 |   |   |   |   |   |   |   |   |");
-printf("\n");
-printf(" —-------------------------------—-- ");
-printf("\n");
-
-printf("| 4 |   |   |   |   |   |   |   |   |");
-printf("\n");
-printf(" —-------------------------------—-- ");
-printf("\n");
-
-printf("| 5 |   |   |   |   |   |   |   |   |");
-printf("\n");
-printf(" —-------------------------------—-- ");
-printf("\n");
-
-printf("| 6 |   |   |   |   |   |   |   |   |");
-printf("\n");
-printf(" —-------------------------------—-- ");
-printf("\n");
-
-printf("| 7 | p | p | p | p | p | p | p | p |");
-printf("\n");
-printf(" —-------------------------------—-- ");
-printf("\n");
-
-printf("| 8 | r | h | e | q | k | e | h | r |");
-printf("\n");
-printf(" —-------------------------------—-- ");
-printf("\n");
-
-printf("|   | a | b | c | d | e | f | g | h |");
-printf("\n");
-printf(" —-------------------------------—-- ");
-printf("\n");
-
-return 0;
+#include<stdio.h>
+int main() {
+    printf("1 R H E Q K E H R\n2 P P P P P P P P\n");
+    printf("3\n4\n5\n6\n7 p p p p p p p p\n");
+    printf("8 r h e q k e h r\n  a b c d e f g h\n\n");
 }
